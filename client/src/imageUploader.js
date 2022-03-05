eaedb8ab2343512e7bcb747e11e67b51fb861c1b
MOH1709 <ahirwalmohit3216@gmail.com>
2022-03-05 18:40:29 +0530

    compressed image before uploading

--- client/src/imageUploader.js
+++ client/src/imageUploader.js
@@ -1,13 +1,22 @@
 import axios from "axios";
 import Cookies from "js-cookie";
+import imageCompression from "browser-image-compression";
 
 const imageUploader = async(file) => {
   if (file) {
     try {
-      const fd = new FormData();
-      const bx = Cookies.get("bx");
-      fd.append("img", file);
+      const options = {
+        maxSizeMB: 2, // (default: Number.POSITIVE_INFINITY)
+        maxWidthOrHeight: 600, // compressedFile will scale down by ratio to a point that width or height is smaller than maxWidthOrHeight (default: undefined)
+        useWebWorker: true, // optional, use multi-thread web worker, fallback to run in main-thread (default: true)
+      };
 
+      const compressedImg = await imageCompression(file, options);
+
+      const fd = new FormData();
+      fd.append("img", compressedImg);
+
+      const bx = Cookies.get("bx");
       const res = await axios.post(`/utils/upload/image/${bx}`, fd, {
         headers: {
           "Content-Type": "multipart/form-data",
